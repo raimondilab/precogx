d5afdfee18b7dbe00c4569f76dd4d92a6122a8c3
gurdeep330 <[gurdeep330@gmail.com]>
2021-11-15 12:00:45 +0100

    added auto PDB workflow

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-import os, sys
+import os, sys, gzip
 
 os.chdir('data/')
 os.system('wget ftp://ftp.ebi.ac.uk/pub/databases/msd/sifts/flatfiles/tsv/pdb_chain_pfam.tsv.gz')
+
+l = ''
+for line in gzip.open('pdb_chain_pfam.tsv.gz', 'rt'):
+    if line[0] != '#' and line.split()[0] != 'PDB':
+        #print (line)
+        pdbid = line.split('\t')[0]
+        pfam = line.split('\t')[3]
+        if pfam == 'PF00001':
+            l += pdbid + '\n'
+
+open('../static/help.txt', 'w').write(l)
