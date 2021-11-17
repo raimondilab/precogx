1071467bc3261439b966a7563d59c53a3eb7f5c3
gurdeep330 <[gurdeep330@gmail.com]>
2021-11-17 14:21:32 +0100

    created home page

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/output', methods=['GET', 'POST'])
+def output():
+    #data = request.form['input']
+    if request.method == 'POST':
+        #print (request.form['input'])
+        path = "static/OL820/out.json"
+        return render_template('embedded.html', name='name', path_to_json_output=json.dumps(path))
+    return ("<h3>It was a GET request</h3>")
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('embedded.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- static/sequence.js
+++ static/sequence.js
@@ -1,48 +1,44 @@
+// Function to read the GPCR name from the Results table and fetch it's FASTA
+// and render it in the Sequence Panel
 function makeSequence(gpcr) {
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function() {
-    var options = this.responseText;
-    options = options.split('>');
-    var new_options = '';
+    var fasta = this.responseText;
+    fasta = fasta.split('>');
     //Ignore i = 0 as it's empty
-    for (var i = 1; i < options.length; i++) {
-      var x = options[i].split('\n');
+    for (var i = 1; i < fasta.length; i++) {
+      var x = fasta[i].split('\n');
+      // Check if the first line has the GPCR name; If yes, selec the sequence
       if (x[0] == gpcr) {
-        //alert ('found');
         var sequence = '';
         for (var j = 1; j < x.length; j++) {
           sequence += x[j]
         }
       }
-      //new_options += "<li><a class=\"dropdown-item\" onClick=\"makeStructure(\'"+pdbid+"\',\'"+chainGPCR+"\',\'"+chainGPROT+"\')\">" + pdbid+"</a></li>";
+
     }
     //alert(sequence);
-    var seq = new Sequence('MALWMRLLPLLALLALWGPGPGAGSLQPLALEGSLQKRGIVEQCCTSICSLYQLENYCN');
+    //var seq = new Sequence('MALWMRLLPLLALLALWGPGPGAGSLQPLALEGSLQKRGIVEQCCTSICSLYQLENYCN');
     var seq = new Sequence(sequence);
     seq.render('#sequence-viewer', {
                 'showLineNumbers': true,
                 'wrapAminoAcids': true,
-                'charsPerLine': 70,
+                'charsPerLine': 60,
                 'toolbar': true,
                 'title' : gpcr,
-                //'search': true,
+                'search': true,
                 'header' : {
                     display:true,
-                    searchInTitle :true,
+                    searchInTitle :false,
                     unit: "Char",
                     showCpl: true,
                     badgeWithUnit : false
                 }
             });
+    //highlight randomly chosen region in the sequence
     seq.selection(21,33,"green");
-    //fasta(sequence);
     //document.getElementById("pdblist").innerHTML = new_options;
   }
   xhttp.open("GET", "static/OL820/temp/new_fasta_file.txt");
   xhttp.send();
-  /*
-}
-function fasta(sequence) {
-  */
-
 }
