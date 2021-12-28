d06296041f5a366a2c53752b5b54d205bcfc30da
gurdeep330 <gurdeep330@gmail.com>
2021-12-28 15:43:52 +0100

    updating templates

--- static/js/index.js
+++ static/js/index.js
@@ -1,36 +1,9 @@
 $( document ).ready(function() {
 
-/* Javascript to show and hide cookie banner using localstorage */
-/* Shows the Cookie banner */
-function showCookieBanner(){
- let cookieBanner = document.getElementById("cb-cookie-banner");
- cookieBanner.style.display = "block";
-}
-
-/* Hides the Cookie banner and saves the value to localstorage */
-function hideCookieBanner(){
- localStorage.setItem("cb_isCookieAccepted", "yes");
- let cookieBanner = document.getElementById("cb-cookie-banner");
- cookieBanner.style.display = "none";
-}
-
-/* Checks the localstorage and shows Cookie banner based on it. */
-function initializeCookieBanner(){
- let isCookieAccepted = localStorage.getItem("cb_isCookieAccepted");
- if(isCookieAccepted === null)
- {
-  localStorage.setItem("cb_isCookieAccepted", "no");
-  showCookieBanner();
- }
- if(isCookieAccepted === "no"){
-  showCookieBanner();
- }
-}
 
 // Assigning values to window object
 window.onload = initializeCookieBanner();
-window.cb_hideCookieBanner = hideCookieBanner;
-
+window.hideCookieBanner = hideCookieBanner;
 
  $(document).on('click', '#ex1', function() {
 
@@ -97,7 +70,32 @@ window.cb_hideCookieBanner = hideCookieBanner;
       });
 });
 
+/* Javascript to show and hide cookie banner using localstorage */
+/* Shows the Cookie banner */
+function showCookieBanner(){
+ let cookieBanner = document.getElementById("cb-cookie-banner");
+ cookieBanner.style.display = "block";
+}
 
+/* Hides the Cookie banner and saves the value to localstorage */
+function hideCookieBanner(){
+ localStorage.setItem("cb_isCookieAccepted", "yes");
+ let cookieBanner = document.getElementById("cb-cookie-banner");
+ cookieBanner.style.display = "none";
+}
+
+/* Checks the localstorage and shows Cookie banner based on it. */
+function initializeCookieBanner(){
+ let isCookieAccepted = localStorage.getItem("cb_isCookieAccepted");
+ if(isCookieAccepted === null)
+ {
+  localStorage.setItem("cb_isCookieAccepted", "no");
+  showCookieBanner();
+ }
+ if(isCookieAccepted === "no"){
+  showCookieBanner();
+ }
+}
 
 
 
