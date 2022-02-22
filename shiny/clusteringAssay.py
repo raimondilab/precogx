094327918fbfd8ff2c396d65b10ebc1877c833ec
gurdeep330 <[gurdeep330@gmail.com]>
2022-02-22 22:20:02 +0100

    close #67

---
+++ shiny/clusteringAssay.py
@@ -0,0 +1,147 @@
+import os, sys
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.metrics.cluster import normalized_mutual_info_score
+import numpy as np
+
+## For Shedding, ebBRET, IUPHAR and STRING
+STRING = ['Barr1-GRK2', 'Barr2', 'Barr2-GRK2']
+ebBRET = ['GNAS', 'GNAI1', 'GNAI2', 'GoA', 'GoB', 'GNAZ', 'GNA12', 'GNA13', 'GNAQ', 'GNA11', 'GNA14', 'GNA15', 'Barr1-GRK2', 'Barr2', 'Barr2-GRK2']
+Shedding = ['GNAS', 'GNAL', 'GNAI1', 'GNAI3', 'GNAZ', 'GNA12', 'GNA13', 'GNAQ', 'GNA14', 'GNA15']
+IUPHAR = list(set(ebBRET+Shedding))
+IUPHAR.remove('Barr2')
+IUPHAR.remove('Barr2-GRK2')
+IUPHAR.remove('Barr1-GRK2')
+
+for dimr in ['UMAP', 'tSNE', 'PCA']:
+    data = []
+    for assay in ['STRING', 'IUPHAR', 'ebBRET', 'Shedding']:
+
+        if assay == 'Shedding':
+            gproteins = Shedding
+        elif assay == 'ebBRET':
+            gproteins = ebBRET
+        elif assay == 'IUPHAR':
+            gproteins = IUPHAR
+        elif assay == 'STRING':
+            gproteins = STRING
+
+        #gproteins = ['GNAS', 'GNA12']
+
+        for gprotein in gproteins:
+            for layer in range(0,34):
+                if dimr == 'PCA':
+                    df = pd.read_csv('all_layers_scaled/'+gprotein+'_All_'+str(layer)+'.tsv', sep='\t')
+                else:
+                    df = pd.read_csv('embedding_all/'+gprotein+'_All_'+str(layer)+'_'+dimr+'.tsv', sep='\t')
+                df[assay].replace({"Coupled": 1, "Not-coupled": 0}, inplace=True)
+                df = df[df[assay] != '-']
+                #print(df[['PC1', 'PC2']].to_numpy())
+                trueLabels = df[assay].to_numpy()
+                #print (trueLabels)
+                X = df[['PC1', 'PC2']].to_numpy()
+                #print (X)
+                kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+                #print (kmeans.labels_)
+                predLabels = kmeans.labels_
+                score = normalized_mutual_info_score(trueLabels, predLabels)
+                print (dimr + '\t' + gprotein + '\t' + str(layer) + '\t' + assay + '\t' + str(round(score,3)))
+
+                row = []
+                row.append(dimr)
+                row.append(assay)
+                row.append(gprotein)
+                row.append(layer)
+                row.append(round(score,3))
+                data.append(row)
+        #sys.exit()
+
+    df = pd.DataFrame(data, columns=['DimR', 'Assay', 'G-protein', 'Layer', 'Score'])
+    df = df.sort_values(by=['Assay', 'G-protein', 'Score'], ascending=[False, False, False])
+    df.to_csv('systematic'+dimr+'.tsv', sep='\t', index=False)
+
+## For classess
+for dimr in ['UMAP', 'tSNE', 'PCA']:
+    data = []
+    assay = "Class"
+    for layer in range(0,34):
+        if dimr == 'PCA':
+            df = pd.read_csv('all_layers_scaled/'+gprotein+'_All_'+str(layer)+'.tsv', sep='\t')
+        else:
+            df = pd.read_csv('embedding_all/'+gprotein+'_All_'+str(layer)+'_'+dimr+'.tsv', sep='\t')
+        df[assay].replace({"classA":0, "classB":1, "classC":2, "Frizzeled":3, "Taste":4}, inplace=True)
+        df = df[df[assay] != 'Other']
+        #print(df[['PC1', 'PC2']].to_numpy())
+        trueLabels = df[assay].to_numpy()
+        #print (trueLabels)
+        X = df[['PC1', 'PC2']].to_numpy()
+        #print (X)
+        kmeans = KMeans(n_clusters=5, random_state=0).fit(X)
+        #print (kmeans.labels_)
+        predLabels = kmeans.labels_
+        score = normalized_mutual_info_score(trueLabels, predLabels)
+        print (str(layer) + '\t' + str(round(score,3)))
+
+        row = []
+        row.append(dimr)
+        row.append(layer)
+        row.append(round(score,3))
+        data.append(row)
+
+    df = pd.DataFrame(data, columns=['DimR', 'Layer', 'Score'])
+    df = df.sort_values(by=['Score'], ascending=[False])
+    df.to_csv('systematicClass'+dimr+'.tsv', sep='\t', index=False)
+
+
+## For families
+'''
+df = pd.read_csv('embedding_all/GNAS_All_0_tSNE.tsv', sep='\t')
+df = df.dropna(subset=['Family'])
+families = list(set(df['Family'].to_numpy()))
+
+dfAllFamilies = pd.DataFrame(columns=['DimR', 'Family', 'Layer', 'Score'])
+for family in families:
+    for dimr in ['UMAP', 'tSNE', 'PCA']:
+        data = []
+        for layer in range(0,34):
+            if dimr == 'PCA':
+                df = pd.read_csv('all_layers_scaled/GNAS_All_'+str(layer)+'.tsv', sep='\t')
+            else:
+                df = pd.read_csv('embedding_all/GNAS_All_'+str(layer)+'_'+dimr+'.tsv', sep='\t')
+
+            #df = df[df['Family'] != NaN]
+            df = df.dropna(subset=['Family'])
+            #print (df.to_numpy())
+            df['FamilyValue'] = np.where(df['Family']== family, 1, 0)
+            #print (df[''])
+            #sys.exit()
+            assay = "FamilyValue"
+            #df[assay].replace({"classA":0, "classB":1, "classC":2, "Frizzeled":3, "Taste":4}, inplace=True)
+            #df = df[df[assay] != 'Other']
+            #print(df[['PC1', 'PC2']].to_numpy())
+            trueLabels = df[assay].to_numpy()
+            #print (trueLabels)
+            X = df[['PC1', 'PC2']].to_numpy()
+            #print (X)
+            kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+            #print (kmeans.labels_)
+            predLabels = kmeans.labels_
+            score = normalized_mutual_info_score(trueLabels, predLabels)
+            print (str(family) + '\t' + str(layer) + '\t' + str(round(score,3)))
+
+            row = []
+            row.append(dimr)
+            row.append(family)
+            row.append(layer)
+            row.append(round(score,3))
+            data.append(row)
+
+        df = pd.DataFrame(data, columns=['DimR', 'Family', 'Layer', 'Score'])
+        df = df.sort_values(by=['Score'], ascending=[False])
+        df.to_csv('systematicFamily/'+family.replace(' ', '_').replace('>', '_').replace('<', '_').replace('/', '_')+'_'+dimr+'.tsv', sep='\t', index=False)
+
+        dfAllFamilies = pd.concat([dfAllFamilies, df])
+        #sys.exit()
+
+dfAllFamilies.to_csv('systematicFamily/AllFamilies.tsv', sep='\t', index=False)
+'''
