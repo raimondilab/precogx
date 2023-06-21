b9da485feb6526729348d9729224a7613e4c925b
gurdeep330 <[gurdeep330@gmail.com]>
2023-06-21 11:35:34 +0200

    installed plotly locally #96

---
+++ static/js/plotly-plotly.js-da2cd8c/test/jasmine/assets/mock_lists.js
@@ -0,0 +1,85 @@
+// list of mocks that should include *all* plotly.js trace modules
+
+var svgMockList = [
+    ['1', require('../../image/mocks/1.json')],
+    ['4', require('../../image/mocks/4.json')],
+    ['5', require('../../image/mocks/5.json')],
+    ['10', require('../../image/mocks/10.json')],
+    ['11', require('../../image/mocks/11.json')],
+    ['17', require('../../image/mocks/17.json')],
+    ['21', require('../../image/mocks/21.json')],
+    ['22', require('../../image/mocks/22.json')],
+    ['airfoil', require('../../image/mocks/airfoil.json')], // important to keep because it's the only mock with config options
+    ['annotations-autorange', require('../../image/mocks/annotations-autorange.json')],
+    ['axes_enumerated_ticks', require('../../image/mocks/axes_enumerated_ticks.json')],
+    ['axes_visible-false', require('../../image/mocks/axes_visible-false.json')],
+    ['bar_and_histogram', require('../../image/mocks/bar_and_histogram.json')],
+    ['waterfall', require('../../image/mocks/waterfall_profit-loss_2018vs2019_rectangle.json')],
+    ['funnel', require('../../image/mocks/funnel_horizontal_group_basic.json')],
+    ['funnelarea', require('../../image/mocks/funnelarea_title_multiple.json')],
+    ['basic_error_bar', require('../../image/mocks/basic_error_bar.json')],
+    ['binding', require('../../image/mocks/binding.json')],
+    ['cheater_smooth', require('../../image/mocks/cheater_smooth.json')],
+    ['finance_style', require('../../image/mocks/finance_style.json')],
+    ['geo_first', require('../../image/mocks/geo_first.json')],
+    ['indicator_bignumber', require('../../image/mocks/indicator_bignumber.json')],
+    ['image_adventurer', require('../../image/mocks/image_adventurer.json')],
+    ['layout_image', require('../../image/mocks/layout_image.json')],
+    ['layout-colorway', require('../../image/mocks/layout-colorway.json')],
+    ['multicategory', require('../../image/mocks/multicategory.json')],
+    ['polar_categories', require('../../image/mocks/polar_categories.json')],
+    ['polar_direction', require('../../image/mocks/polar_direction.json')],
+    ['polar_wind-rose', require('../../image/mocks/polar_wind-rose.json')],
+    ['range_selector_style', require('../../image/mocks/range_selector_style.json')],
+    ['range_slider_multiple', require('../../image/mocks/range_slider_multiple.json')],
+    ['sankey_energy', require('../../image/mocks/sankey_energy.json')],
+    ['sunburst_coffee', require('../../image/mocks/sunburst_coffee.json')],
+    ['treemap_coffee', require('../../image/mocks/treemap_coffee.json')],
+    ['icicle_coffee', require('../../image/mocks/icicle_coffee.json')],
+    ['parcats_bad-displayindex', require('../../image/mocks/parcats_bad-displayindex.json')],
+    ['scattercarpet', require('../../image/mocks/scattercarpet.json')],
+    ['smith_basic', require('../../image/mocks/smith_basic.json')],
+    ['shapes', require('../../image/mocks/shapes.json')],
+    ['splom_iris', require('../../image/mocks/splom_iris.json')],
+    ['table_wrapped_birds', require('../../image/mocks/table_wrapped_birds.json')],
+    ['ternary_fill', require('../../image/mocks/ternary_fill.json')],
+    ['text_chart_arrays', require('../../image/mocks/text_chart_arrays.json')],
+    ['transforms', require('../../image/mocks/transforms.json')],
+    ['updatemenus', require('../../image/mocks/updatemenus.json')],
+    ['violin_side-by-side', require('../../image/mocks/violin_side-by-side.json')],
+    ['world-cals', require('../../image/mocks/world-cals.json')],
+    ['typed arrays', {
+        data: [{
+            x: new Float32Array([1, 2, 3]),
+            y: new Float32Array([1, 2, 1])
+        }]
+    }]
+];
+
+var glMockList = [
+    ['gl2d_heatmapgl', require('../../image/mocks/gl2d_heatmapgl.json')],
+    ['gl2d_line_dash', require('../../image/mocks/gl2d_line_dash.json')],
+    ['gl2d_parcoords_2', require('../../image/mocks/gl2d_parcoords_2.json')],
+    ['gl2d_pointcloud-basic', require('../../image/mocks/gl2d_pointcloud-basic.json')],
+    ['gl3d_annotations', require('../../image/mocks/gl3d_annotations.json')],
+    ['gl3d_set-ranges', require('../../image/mocks/gl3d_set-ranges.json')],
+    ['gl3d_world-cals', require('../../image/mocks/gl3d_world-cals.json')],
+    ['gl3d_cone-autorange', require('../../image/mocks/gl3d_cone-autorange.json')],
+    ['gl3d_streamtube-simple', require('../../image/mocks/gl3d_streamtube-simple.json')],
+    ['glpolar_style', require('../../image/mocks/glpolar_style.json')],
+    ['gl3d_isosurface_multiple-traces', require('../../image/mocks/gl3d_isosurface_multiple-traces.json')],
+    ['gl3d_volume_multiple-traces', require('../../image/mocks/gl3d_volume_multiple-traces.json')]
+];
+
+var mapboxMockList = [
+    ['scattermapbox', require('../../image/mocks/mapbox_bubbles-text.json')],
+    ['choroplethmapbox', require('../../image/mocks/mapbox_choropleth0.json')],
+    ['densitymapbox', require('../../image/mocks/mapbox_density0.json')]
+];
+
+module.exports = {
+    svg: svgMockList,
+    gl: glMockList,
+    mapbox: mapboxMockList,
+    all: svgMockList.concat(glMockList).concat(mapboxMockList)
+};
---
+++ static/js/plotly-plotly.js-da2cd8c/test/jasmine/tests/download_test.js
@@ -0,0 +1,199 @@
+var Plotly = require('../../../lib/index');
+var Lib = require('../../../src/lib');
+
+var createGraphDiv = require('../assets/create_graph_div');
+var destroyGraphDiv = require('../assets/destroy_graph_div');
+
+
+var textchartMock = require('../../image/mocks/text_chart_arrays.json');
+var LONG_TIMEOUT_INTERVAL = 2 * jasmine.DEFAULT_TIMEOUT_INTERVAL;
+
+describe('Plotly.downloadImage', function() {
+    var gd;
+
+    var createElement = document.createElement;
+    var slzProto = (new window.XMLSerializer()).__proto__;
+    var serializeToString = slzProto.serializeToString;
+
+    beforeEach(function() {
+        gd = createGraphDiv();
+
+        // override click handler on createElement
+        //  so these tests will not actually
+        //  download an image each time they are run
+        //  full credit goes to @etpinard; thanks
+        spyOn(document, 'createElement').and.callFake(function(args) {
+            var el = createElement.call(document, args);
+            el.click = function() {};
+            return el;
+        });
+    });
+
+    afterEach(function() {
+        destroyGraphDiv();
+        delete window.navigator.msSaveBlob;
+    });
+
+    it('should be attached to Plotly', function() {
+        expect(Plotly.downloadImage).toBeDefined();
+    });
+
+    it('should create link, remove link, accept options', function(done) {
+        downloadTest(gd, 'jpeg')
+        .then(done, done.fail);
+    }, LONG_TIMEOUT_INTERVAL);
+
+    it('should create link, remove link, accept options', function(done) {
+        downloadTest(gd, 'png')
+        .then(done, done.fail);
+    }, LONG_TIMEOUT_INTERVAL);
+
+    it('should create link, remove link, accept options', function(done) {
+        downloadTest(gd, 'full-json')
+        .then(done, done.fail);
+    }, LONG_TIMEOUT_INTERVAL);
+
+    it('should create link, remove link, accept options', function(done) {
+        checkWebp(function(supported) {
+            if(supported) {
+                downloadTest(gd, 'webp')
+                .then(done, done.fail);
+            } else {
+                done();
+            }
+        });
+    }, LONG_TIMEOUT_INTERVAL);
+
+    it('should create link, remove link, accept options', function(done) {
+        downloadTest(gd, 'svg')
+        .then(done, done.fail);
+    }, LONG_TIMEOUT_INTERVAL);
+
+    it('should work when passing graph div id', function(done) {
+        downloadTest('graph', 'svg')
+        .then(done, done.fail);
+    }, LONG_TIMEOUT_INTERVAL);
+
+    it('should work when passing a figure object', function(done) {
+        var fig = {
+            data: [{y: [1, 2, 1]}]
+        };
+        Plotly.downloadImage(fig)
+        .then(function() {
+            expect(document.createElement).toHaveBeenCalledWith('canvas');
+            expect(gd._snapshotInProgress)
+                .toBe(undefined, 'should not attach _snapshotInProgress to figure objects');
+        })
+        .then(done, done.fail);
+    }, LONG_TIMEOUT_INTERVAL);
+
+    it('should produce the right SVG output in IE', function(done) {
+        // mock up IE behavior
+        spyOn(Lib, 'isIE').and.callFake(function() { return true; });
+        spyOn(slzProto, 'serializeToString').and.callFake(function() {
+            return serializeToString.apply(this, arguments)
+                .replace(/(\(#)([^")]*)(\))/gi, '(\"#$2\")');
+        });
+        var savedBlob;
+        window.navigator.msSaveBlob = function(blob) { savedBlob = blob; };
+
+        var expectedStart = '<svg class=\'main-svg\' xmlns=\'http://www.w3.org/2000/svg\' xmlns:xlink=\'http://www.w3.org/1999/xlink\'';
+        var plotClip = /clip-path='url\("#clip[0-9a-f]{6}xyplot"\)/;
+        var legendClip = /clip-path=\'url\("#legend[0-9a-f]{6}"\)/;
+
+        Plotly.newPlot(gd, textchartMock.data, textchartMock.layout)
+        .then(function(gd) {
+            savedBlob = undefined;
+            return Plotly.downloadImage(gd, {
+                format: 'svg',
+                height: 300,
+                width: 300,
+                filename: 'plotly_download'
+            });
+        })
+        .then(function() {
+            if(savedBlob === undefined) {
+                fail('undefined saveBlob');
+            }
+
+            return new Promise(function(resolve, reject) {
+                var reader = new FileReader();
+                reader.onloadend = function() {
+                    var res = reader.result;
+
+                    expect(res.substr(0, expectedStart.length)).toBe(expectedStart);
+                    expect(res.match(plotClip)).not.toBe(null);
+                    expect(res.match(legendClip)).not.toBe(null);
+
+                    resolve();
+                };
+                reader.onerror = function(e) { reject(e); };
+
+                reader.readAsText(savedBlob);
+            });
+        })
+        .then(done, done.fail);
+    }, LONG_TIMEOUT_INTERVAL);
+});
+
+function downloadTest(gd, format) {
+    // use MutationObserver to monitor the DOM
+    // for changes
+    // code modeled after
+    // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+    // select the target node
+    var target = document.body;
+    var domchanges = [];
+
+    // create an observer instance
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            domchanges.push(mutation);
+        });
+    });
+
+    return Plotly.newPlot(gd, textchartMock.data, textchartMock.layout).then(function(_gd) {
+        // start observing dom
+        // configuration of the observer:
+        var config = { childList: true };
+
+        // pass in the target node and observer options
+        observer.observe(target, config);
+
+        var promise = Plotly.downloadImage(gd, {
+            format: format,
+            height: 300,
+            width: 300,
+            filename: 'plotly_download'
+        });
+
+        expect(_gd._snapshotInProgress).toBe(true, 'should attach _snapshotInProgress to graph divs');
+
+        return promise;
+    })
+    .then(function(filename) {
+        observer.disconnect();
+
+        // look for an added and removed link
+        var linkadded = domchanges[domchanges.length - 2].addedNodes[0];
+        var linkdeleted = domchanges[domchanges.length - 1].removedNodes[0];
+
+        expect(linkadded.getAttribute('href').split(':')[0]).toBe('blob');
+        expect(filename).toBe('plotly_download.' + format.replace('-', '.'));
+        expect(linkadded).toBe(linkdeleted);
+    });
+}
+
+// Only chrome supports webp at the time of writing
+function checkWebp(cb) {
+    var img = new Image();
+    img.onload = function() {
+        cb(true);
+    };
+
+    img.onerror = function() {
+        cb(false);
+    };
+
+    img.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+}
